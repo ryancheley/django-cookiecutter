2382b36eb40c0e0b3ab051d0927602b2f7507ddb
Ryan Cheley <rcheley@gmail.com>
2021-11-21 15:54:27 -0800

    added django admin theme instructions

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,3 +3,9 @@ echo "Rename the example .env file"
 mv {{cookiecutter.project_slug}}/.example_env {{cookiecutter.project_slug}}/.env
 
 mkdir staticfiles
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+python manage.py loaddata admin_interface_themes.json
