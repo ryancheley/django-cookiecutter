554880f11080978dc3e7d7266ee74b86d67f2e2c
Ryan Cheley <rcheley@gmail.com>
2021-11-21 16:27:04 -0800

    adding commands to do more setup

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,10 +2,17 @@
 echo "Rename the example .env file"
 mv {{cookiecutter.project_slug}}/.example_env {{cookiecutter.project_slug}}/.env
 
+echo "Make the staticfiles directory"
 mkdir staticfiles
 
+echo "Create the virtual environment"
 python3 -m venv venv
 
+echo "Activate the Virtual Environment"
 source venv/bin/activate
 
+echo "Install requirements"
+pip install -r requirements/local/txt
+
+echo "Load the admin interface themes"
 python manage.py loaddata admin_interface_themes.json
