7a70681bc0ad5e1c491c2cddb1c1a24729dc7146
Ryan Cheley <rcheley@gmail.com>
2022-02-06 06:51:11 -0800

    Fix: Virtual Environment Creation Issue

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+
 echo "Rename the example .env file"
 mv {{cookiecutter.project_slug}}/.example_env {{cookiecutter.project_slug}}/.env
+mv {{cookiecutter.project_slug}}/.prod_example_env {{cookiecutter.project_slug}}/.env_prod
 
 echo "Make the staticfiles directory"
 mkdir staticfiles
 
-echo "Create the virtual environment"
-python3 -m venv venv
-
-echo "Activate the Virtual Environment"
-source venv/bin/activate
-
 echo "Install requirements"
+pip install -U pip
+pip install -r requirements.txt
 pip install -r requirements/local.txt
 
 echo "Run migrations"
@@ -19,3 +17,6 @@ python manage.py migrate
 
 echo "Load the admin interface themes"
 python manage.py loaddata admin_interface_themes.json
+
+echo "Initial Git Repo"
+git init
